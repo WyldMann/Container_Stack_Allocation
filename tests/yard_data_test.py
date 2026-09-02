3a96540dc05ab131d11043dfca41211df4fb7dc6
christopher.edgar <christopher.edgar@meratusline.com>
2026-09-02 15:54:47 +0700

    change using prod data sample

--- tests/yard_data_test.py
+++ tests/yard_data_test.py
@@ -8,12 +8,13 @@ removed_slot_file = Path(__file__).parent.parent / "data" / "_TRM_MST_YARD_BLOCK
 parameter_file = Path(__file__).parent.parent / "data" / "_TRM_MST_YARD_PARAMETER__202608271123.csv"
 yard_planning_input_file =  Path(__file__).parent.parent / "data" / "_TRM_TRS_YARD_PLANNING_ITEM__202608271125.csv"
 container_file = Path(__file__).parent.parent / "data" / "_TRM_TRS_TARGET_CONTAINER.csv"
+prod_container_file = Path(__file__).parent.parent / "data" / "rpt-terminalapp-010926.csv"
 
 yard_repo = Repository(yardfile).readCSV()
 removed_slot = RemovedSlotsRepository(removed_slot_file).readCSVTuple()
 parameter_input = Repository(parameter_file).readCSV()
 yard_planning_input = YardPlanningRepository(yard_planning_input_file).readCSVDictTuple()
-container_repo = Repository(container_file).readCSV()
+container_repo = Repository(prod_container_file).readCSV()
 
 yard = Yard(yard_repo,removed_slot,parameter_input,yard_planning_input,container_repo)
 
