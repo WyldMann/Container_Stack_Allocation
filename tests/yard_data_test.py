2447e874ccff050197664ff48e73ec252222524e
christopher.edgar <christopher.edgar@meratusline.com>
2026-09-02 14:27:01 +0700

    container data test input

--- tests/yard_data_test.py
+++ tests/yard_data_test.py
@@ -7,13 +7,15 @@ yardfile = Path(__file__).parent.parent / "data"/ "_TRM_MST_YARD_BLOCK__20260827
 removed_slot_file = Path(__file__).parent.parent / "data" / "_TRM_MST_YARD_BLOCK_REMOVED_SLOT__202608271123.csv"
 parameter_file = Path(__file__).parent.parent / "data" / "_TRM_MST_YARD_PARAMETER__202608271123.csv"
 yard_planning_input_file =  Path(__file__).parent.parent / "data" / "_TRM_TRS_YARD_PLANNING_ITEM__202608271125.csv"
+container_file = Path(__file__).parent.parent / "data" / "_TRM_TRS_TARGET_CONTAINER.csv"
 
 yard_repo = Repository(yardfile).readCSV()
 removed_slot = RemovedSlotsRepository(removed_slot_file).readCSVTuple()
 parameter_input = Repository(parameter_file).readCSV()
 yard_planning_input = YardPlanningRepository(yard_planning_input_file).readCSVDictTuple()
+container_repo = Repository(container_file).readCSV()
 
-yard = Yard(yard_repo,removed_slot,parameter_input,yard_planning_input)
+yard = Yard(yard_repo,removed_slot,parameter_input,yard_planning_input,container_repo)
 
 yard.print()
 yard.getBadData().printYPOutOfRange()
