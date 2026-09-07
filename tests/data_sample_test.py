6f1d2fab8d81486918dcba3f844bace798db2495
christopher.edgar <christopher.edgar@meratusline.com>
2026-09-07 10:47:06 +0700

    sample data

---
+++ tests/data_sample_test.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from filereader import FileReader, RemovedSlotsFileReader, YardPlanningFileReader, ContainerFileReader
+from entities import Yard
+
+blockFile = Path(__file__).parent / "test_data" / "yard_block.csv"
+ypFile = Path(__file__).parent / "test_data" / "yard_planning.csv"
+removedFile = Path(__file__).parent / "test_data" / "removed_slots.csv"
+parameterFile = Path(__file__).parent / "test_data" / "parameters.csv"
+
+block_repo = FileReader(blockFile).readCSV()
+removed_repo = RemovedSlotsFileReader(removedFile).readCSVTuple()
+parameter_repo = FileReader(parameterFile).readCSV()
+yp_repo = YardPlanningFileReader(ypFile).readCSVDictTuple()
+
+print(parameter_repo[0])
+
+yard = Yard(block_repo,
+            removed_repo,
+            parameter_repo,
+            yp_repo)
+yard.print()
