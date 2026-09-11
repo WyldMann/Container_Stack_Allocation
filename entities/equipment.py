763a2c7545b354e94f85e24fa9b19cf3db3a2980
christopher.edgar <christopher.edgar@meratusline.com>
2026-09-11 11:07:34 +0700

    moveRTG methods to superclass and fixes

--- entities/equipment.py
+++ entities/equipment.py
@@ -12,6 +12,10 @@ class Equipment:
     def getCode(self) -> str: return self.code
     def getCoords(self) -> tuple[str,int,int]: return self.coords
     def getCoordsStr(self) -> tuple[str,str,str]: return self.coords[0],str(self.coords[1] + 1),str(self.coords[2] + 1)
+    def inBlockMove (self, row:int, slot:int):
+        self.coords = (self.coords[0],row,slot)
+    def inBlockDistance (self, row:int, slot:int) -> float:
+        return math.sqrt((row - self.coords[1]) ** 2 + (slot - self.coords[2]) ** 2)
 
 class RTG(Equipment):
     def __init__(self,code:str,coords:tuple[str,int,int]) -> None:
