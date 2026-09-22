de4a22e5d661fb353defa24945a47819ee18653e
christopher.edgar <christopher.edgar@meratusline.com>
2026-09-22 08:40:28 +0700

    fixed inconsistent slot-row format

--- utils/coord_handling.py
+++ utils/coord_handling.py
@@ -2,5 +2,5 @@ def toIDSlotRowTier(coord: tuple[str,str,str,str])->tuple[str,str,str,str]:
     return coord[0], coord[2], coord[1], coord[3]
 def toIDSlotRow(coord: tuple[str,str,str]) -> tuple[str,str,str]:
     return coord[0], coord[2], coord[1]
-def strRowSlotTier(coord: tuple[str,int,int,int]) -> tuple[str,str,str,str]:
+def toStrSlotRowTier(coord: tuple[str,int,int,int]) -> tuple[str,str,str,str]:
     return coord[0], str(coord[2] + 1), str(coord[1] + 1), str(coord[3] + 1)
