db23e2947c90374b8590a7433b7100d777db3508
christopher.edgar <christopher.edgar@meratusline.com>
2026-09-23 16:07:32 +0700

    defined min and max value

--- entities/heuristic_model.py
+++ entities/heuristic_model.py
@@ -3,28 +3,34 @@ from dataclasses import dataclass
 
 @dataclass
 class HeuristicModel:
-    wDeltaWeight = 1
+    wDeltaWeight = 10
     hlDeltaWeight = 5000
 
     lambdaDeltaWeight = math.log(2) / hlDeltaWeight
 
-    wEquipmentDistance = 1
+    wEquipmentDistance = 10
     hlEquipmentDistance = 5
 
     lambdaEquipmentDistance = math.log(2) / hlEquipmentDistance
+
+    min_value = 0.001
+    no_delta_weight_value = 0.1
 
     @staticmethod
     def decayFormula(lambda_: float, variable:float) -> float:
         return math.exp(-lambda_*variable)
 
-    def deltaWeight(self,deltaWeight:float):
+    def deltaWeight(self,deltaWeight:float | None):
+        if deltaWeight is None:
+            return self.no_delta_weight_value
         if deltaWeight < 0:
-            return 0.01
+            return self.min_value
+
         else: return self.wDeltaWeight * self.decayFormula(self.lambdaDeltaWeight,deltaWeight)
 
     def equipmentDistance(self,equipmentDistance:float):
         if equipmentDistance == float('inf'):
-            return 0.01
+            return self.min_value
         else:
             return self.wEquipmentDistance * self.decayFormula(self.lambdaEquipmentDistance,equipmentDistance)
 
